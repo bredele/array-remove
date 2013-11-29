f4647847723a88c9398fb515c55ba7cc40de64cb
bredele <olivier.wietrich@gmail.com>
2013-11-29 13:37:54 -0700

    first draft using unshift

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function(arr, cb) {
 	var l = arr.length,
 			cache = [];
 	while(l--) {
-		if(cb(arr[l])) cache.push(arr.splice(l,1));
+		if(cb(arr[l])) cache.unshift(arr.splice(l,1)[0]);
 	}
 	return cache;
 };
