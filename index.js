c6009a15b4b86cc0481cc583189214e37a1f0127
bredele <olivier.wietrich@gmail.com>
2013-11-29 13:54:38 -0700

    update doc

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 
 /**
- * Expose 'Mod'
+ * Remove.
+ * @param {Array} arr 
+ * @param {Function} cb 
+ * @return {Array} array of removes items
+ * @api public
  */
 
 module.exports = function(arr, cb) {
-	var l = arr.length,
-			cache = [];
-	while(l--) {
-		if(cb(arr[l])) cache.unshift(arr.splice(l,1)[0]);
-	}
-	return cache;
+  var l = arr.length,
+      cache = [];
+  while(l--) {
+    if(cb(arr[l])) cache.unshift(arr.splice(l,1)[0]);
+  }
+  return cache;
 };
